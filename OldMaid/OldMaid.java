52ad0b93d3464791f5814226a11cfb740544336a
ching-yu <chia56028@gmail.com>
2017-04-17 17:01:12 +0800

    first edition

--- OldMaid/OldMaid.java
+++ OldMaid/OldMaid.java
@@ -3,20 +3,42 @@ import java.util.*;
 public class OldMaid{
 	public static Deck deck=new Deck();
 	public static Player players[]=new Player[4];
+	public static int price=1;
 
 	public static void main(String args[]){
 		initializePlayers();
 		shuffleAndDeal();
+
 		for(int round=0; round<100; round++){
 			for(int i=0; i<4; i++) eliminateAndCount(i);
+
 			if(players[0].numberOfPokers==0){
-				System.out.println("You win the game!!!");
+				do{
+					for(int i=1; i<4; i++){
+						if(players[i].numberOfPokers==0) continue;
+						int j=1;
+						while(players[(i+j)%4].numberOfPokers==0) j++;
+						draw(i,(i+j)%4);
+					}
+					if(price==4){
+						for(int k=0; k<4; k++){
+							if(players[k].place==0){
+								players[k].place=4;
+								price++;
+							}
+						}
+					}
+				}while(price!=5);
+				for(int i=0; i<4; i++) show(i);
+				System.out.println("You got "+players[0].place+" place.");
 				break;
 			}
 			if(players[1].numberOfPokers==0 && players[2].numberOfPokers==0 && players[3].numberOfPokers==0){
+				for(int i=0; i<4; i++) show(i);
 				System.out.println("You lose the game.");
 				break;
 			}
+
 			for(int i=0; i<4; i++) show(i);
 			for(int i=0; i<4; i++){
 				if(players[i].numberOfPokers==0) continue;
@@ -42,6 +64,7 @@ public class OldMaid{
 			players[i].eliminate();
 			players[i].sort();
 			players[i].count();
+			players[i].shuffle(players[i].numberOfPokers);
 	}
 
 	public static void draw(int i, int j){
@@ -55,13 +78,16 @@ public class OldMaid{
 		Scanner scn=new Scanner(System.in);
 		int input=99;
 		boolean check=false;
-		/*
-		System.out.println("Player 1 ("+players[i].numberOfPokers+" pokers)");
+		
+		System.out.println("Your pokers :");
+		players[i].sort();
+		for(int k=0; k<15; k++) players[i].handPokers[k]=new Poker(players[i].handPokers[k].serialNumber);
 		players[i].showPoker();
-		*/
+		
 		System.out.println("Next player have "+players[j].numberOfPokers+" pokers.");
 		while(check==false){
-			System.out.println("Please input a number between 1 - "+players[j].numberOfPokers);
+			System.out.print("Please input a number between 1 - "+players[j].numberOfPokers+" :");
+			System.out.println();
 			input=scn.nextInt();
 			if(input>=1 && input<=players[j].numberOfPokers) check=true;
 		}
@@ -77,11 +103,28 @@ public class OldMaid{
 		players[i].count();
 		players[j].sort();
 		players[j].count();
+		players[i].shuffle(players[i].numberOfPokers);
+		players[j].shuffle(players[j].numberOfPokers);
+		if(players[j].numberOfPokers==0){
+			if(players[j].place==0){
+				players[j].place=price;
+				price++;
+			}
+		}
+		if(players[i].numberOfPokers==0){
+			if(players[i].place==0){
+				players[i].place=price;
+				price++;
+			}
+		}				
 	}
 
 	public static void show(int i){
-		System.out.println("Player "+i+" ("+players[i].numberOfPokers+" pokers)");
-		players[i].showPoker();
+		if(players[i].place!=0){
+			System.out.println("Player "+(i+1)+" : "+players[i].place+" place");
+		}else{
+			System.out.println("Player "+(i+1)+" ("+players[i].numberOfPokers+" pokers)");
+		}
 	}
 }
 
@@ -94,6 +137,41 @@ class Poker{
 		serialNumber=99;
 		suit="NULL";
 		rank="NULL";
+	}
+
+	Poker(int ser){
+		serialNumber=ser;
+		suit=setSuit(ser);
+		rank=setNumber(ser);
+	}
+
+	public static String setSuit(int ord){
+		if(ord==52 || ord==53) return "Joker";
+		if(ord/13==0){
+			return "Club";
+		}else if(ord/13==1){
+			return "Diamond";
+		}else if(ord/13==2){
+			return "Heart";
+		}else{
+			return "Spades";
+		}
+	}
+
+	public static String setNumber(int ord){
+		if(ord==52 || ord==53) return "Joker";
+		ord+=1;
+		if(ord%13==1){
+			return "A";
+		}else if(ord%13==11){
+			return "J";
+		}else if(ord%13==12){
+			return "Q";
+		}else if(ord%13==0){
+			return "K";
+		}else{
+			return Integer.toString(ord%13);
+		}
 	}
 }
 
@@ -135,8 +213,10 @@ class Deck{
 class Player{
 	int numberOfPokers;
 	Poker handPokers[]=new Poker[15];
+	int place;
 
 	Player(){
+		place=0;
 		numberOfPokers=15;
 		for(int i=0; i<15; i++) handPokers[i]=new Poker();
 	}
@@ -147,7 +227,7 @@ class Player{
 				System.out.println();
 				return;
 			}
-			System.out.print(handPokers[i].serialNumber+"\t");
+			System.out.println(handPokers[i].serialNumber+"\t"+handPokers[i].suit+"\t"+handPokers[i].rank);
 		}
 	}
 
@@ -188,6 +268,18 @@ class Player{
 				//System.out.println("number : "+numberOfPokers);
 				return;
 			}
+		}
+	}
+
+	public void shuffle(int num){
+		Random rand=new Random(System.currentTimeMillis());
+		int n, temp;
+
+		for(int i=0; i<num; i++){
+			n=rand.nextInt(num);
+			temp=handPokers[i].serialNumber;
+			handPokers[i].serialNumber=handPokers[n].serialNumber;
+			handPokers[n].serialNumber=temp;
 		}
 	}
 }
